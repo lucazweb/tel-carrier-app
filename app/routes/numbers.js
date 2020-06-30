007c8be7db370bac9150e6cabee7bdace0f6204a
Lucas Santos <lucazwebmail@gmail.com>
2020-06-29 21:21:24 -0300

    feat: add number route

--- app/routes/numbers.js
+++ app/routes/numbers.js
@@ -1,8 +1,22 @@
 const data = require('./numbers.mock.json');
+
 module.exports = (app) => {
   app.get('/numbers', (req, res) => {
     res.json(data);
   });
+
+  app.get('/number/:id', (req, res) => {
+    console.log(req.params);
+    const { id } = req.params;
+    const [query] = data.filter((number) => number.id === parseInt(id));
+    res.json(query);
+  });
+
+  // save numbers
+
+  // update numbers
+
+  // delete numbers
 
   return app;
 };
