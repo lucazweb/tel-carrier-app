90dd6359bacde234fa0ff92e0a99502738d13806
Lucas Santos <lucazwebmail@gmail.com>
2020-07-02 00:19:59 -0300

    feat: create modal component

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export * from './Header/header.component';
 export * from './Table/table.component';
 export * from './Table/pagination.component';
 export * from './Loading/loading.component';
+export * from './Modal/modal.component';
